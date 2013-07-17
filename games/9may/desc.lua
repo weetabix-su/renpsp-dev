767ba00a0b0ac374056eb5f5105a34115a51456d
Mikko Luis Saavedra <vovo27_miranemiko@yahoo.co.jp>
2013-07-18 06:55:05 +0800

    Additional desc text for 9may

--- games/9may/desc.lua
+++ games/9may/desc.lua
@@ -1,6 +1,6 @@
 gamedesc = {
 	title = "9 мая",
 	auth = "iichan Eroge Team",
-	desc = "",
+	desc = "Небольшой сувенир в честь праздника от одного из участников команды.",
 	screen = "sample.jpg"
 }
