a7dec5ae157430c9dbd69b69359473433c501bc2
Mikko Luis Saavedra <vovo27_miranemiko@yahoo.co.jp>
2013-07-18 06:49:12 +0800

    Description files and screenshots

---
+++ games/9may/desc.lua
@@ -0,0 +1,6 @@
+gamedesc = {
+	title = "9 мая",
+	auth = "iichan Eroge Team",
+	desc = "",
+	screen = "sample.jpg"
+}
---
+++ games/test/desc.lua
@@ -0,0 +1,6 @@
+gamedesc = {
+	title = "RenPSP Test Script",
+	auth = "lolbot & weetabix",
+	desc = "Scripts to test certain functions of RenPSP.",
+	screen = nil
+}
