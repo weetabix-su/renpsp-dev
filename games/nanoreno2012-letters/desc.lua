1a76b746288125e4a03fc55319edd43dd84df928
Mikko Luis Saavedra <vovo27_miranemiko@yahoo.co.jp>
2013-09-12 17:27:19 +0800

    Description data for "Letters for You"

---
+++ games/nanoreno2012-letters/desc.lua
@@ -0,0 +1,6 @@
+gamedesc = {
+	title = "Letters For You",
+	auth = "Patty",
+	desc = "[NANORENO 2012] A story about Tegami, a special mail carrier, and Sayaka, a girl who stubbornly refuses to receive anything he delivers.",
+	screen = "sample.jpg"
+}
